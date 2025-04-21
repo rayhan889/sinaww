f2ec337e62f5ed7229328abde304de0fbfdd79c0
rayhan889 <raydev1604@gmail.com>
2025-04-21 10:34:44 +0700

    fix eslint issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Rubik } from 'next/font/google'
 import './globals.css'
 
-import { Providers } from '@/components/Providers'
+import { Providers } from '@/components/providers'
 
 const rubik = Rubik({
   subsets: ['latin']
