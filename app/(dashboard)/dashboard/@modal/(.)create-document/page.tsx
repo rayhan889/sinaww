73e6f56ab7d61e63b0522edf22660e3769b590b0
rayhan889 <raydev1604@gmail.com>
2025-04-22 17:18:03 +0700

    force dynamic for intercep modal

--- app/(dashboard)/dashboard/@modal/(.)create-document/page.tsx
+++ app/(dashboard)/dashboard/@modal/(.)create-document/page.tsx
@@ -3,6 +3,8 @@ import { getAuthSession } from '@/lib/auth'
 import DialogPortal from '@/components/DialogPortal'
 import CreateDocForm from '@/components/CreateDocForm'
 
+export const dynamic = 'force-dynamic'
+
 export default async function CreateDocumentModal() {
   const session = await getAuthSession()
 
